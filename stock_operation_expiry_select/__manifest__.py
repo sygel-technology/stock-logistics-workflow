aacf1be7efa16a0bea30bc4d31c75bc3e2bd05ec
manu <manuel.regidor@sygel.es>
2022-05-12 16:53:28 +0200

    [ADD]stock_operation_expiry_select: Always select expiration date in incoming stock moves when possible

---
+++ stock_operation_expiry_select/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Operation Expiry Select",
+    "summary": "Select expiry date for all out pickings with lots/serial numbers.",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "depends": ["product_expiry"],
+    "data": ["views/stock_move_views.xml"],
+}
---
+++ stock_operation_expiry_select/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module allows to select an expiration date in stock
+move lines in incoming stock moves when the product accepts
+expiration dates and the picking type has the option
+'Use Existing Lots/Serial Numbers' selected.
